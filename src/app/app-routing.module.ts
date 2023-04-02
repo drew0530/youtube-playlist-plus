ea88a61c4f64f6c05ea85f48d52f85a27e2f2a32
drew0530 <drewwilliamson0709@flpoly.org>
2023-04-02 17:55:10 -0400

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+import { PlayerComponent } from './player/player.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), YouTubePlayerModule, PlayerComponent],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
